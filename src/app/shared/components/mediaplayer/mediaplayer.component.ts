053846ecfcce3076884c6be4b243704927f225b7
Jesus Daniel Bautista Banda <coreyjordison184@gmail.com>
2022-02-03 23:45:33 -0600

    NG-TEMPLATE

--- src/app/shared/components/mediaplayer/mediaplayer.component.ts
+++ src/app/shared/components/mediaplayer/mediaplayer.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mediaplayer.component.css']
 })
 export class MediaplayerComponent implements OnInit {
-
+  mockCover:any={
+    cover:'https://m.media-amazon.com/images/I/71mlUmU1TVL._AC_SL1500_.jpg',
+    name:'Nuthin But a G Thang',
+    album:'Dr. Dre'
+  }
   constructor() { }
 
   ngOnInit(): void {
